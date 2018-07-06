82a31b3e1feffe713bb39f41ee678d7b4964be4d
hampsonn <charcole@pm.me>
2018-07-06 17:15:58 -0500

    components in place, not updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import CountryMenu from './CountryMenu'
+
 import FlyoverCalc from './FlyoverCalc'
 
 /*
@@ -18,40 +18,102 @@ Separately, display current ISS coordinates
 */
 
 const battutaKey = "ad3486b7c0cf4595f01223963f58f91a";
+const battutaURL = "http://battuta.medunes.net/api/";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
-      listLoaded: false,
+      countryLoaded: false,
+      regionLoaded: false,
+      cityLoaded: false,
       passLoaded: false,
       nextPass: "",
       countryList: [],
+      regionList: [],
+      cityList: [],
+      selectedCountry: ["Chad", "td"],
+      selectedRegion: "Chari-Baguirmi Region",
+      selectedCity: "Baguirmi",
     };
+    this.handleCountry = this.handleCountry.bind(this);
   };
 
+  handleCountry(event) {
+    const tempArray = event.target.value.split(',');
+    const tempO = {
+      name: tempArray[0],
+      code: tempArray[1]
+    }
+    this.setState({ selectedCountry: tempO });
+  }
+  handleRegion(event) {
+    this.setState({ selectedRegion: event.target.value });
+  }
+  handleCity(event) {
+    this.setState({ selectedCity: event.target.value });
+  }
+
   componentDidMount() {
-    fetch("http://battuta.medunes.net/api/country/all/?key="+battutaKey)
+    fetch(battutaURL+"country/all/?key="+battutaKey)
           .then(res => res.json())
           .then(
             (result) => {
-              const tempArray = result.map((item) => {
-                return Object.values(item);
-              })
-              console.log(tempArray);
               this.setState({
-                listLoaded: true,
-                countryList: tempArray,
+                countryLoaded: true,
+                countryList: result,
               });
-              console.log(this.state.countryList[0])
             },
             // Note: it's important to handle errors here
             // instead of a catch() block so that we don't swallow
             // exceptions from actual bugs in components.
             (error) => {
               this.setState({
-                listLoaded: true,
+                countryLoaded: true,
+                error
+              });
+            }
+          )
+    fetch(battutaURL+"region/"+this.state.selectedCountry[1]+"/all/?key="+battutaKey)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              const tempArray = result.map((item) => {
+                return Object.values(item);
+              })
+              this.setState({
+                regionLoaded: true,
+                regionList: tempArray,
+              });
+              console.log(this.state.regionList)
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                regionLoaded: true,
+                error
+              });
+            }
+          )
+    fetch(battutaURL+"city/"+this.state.selectedCountry[1]+"/search/?region="+this.state.selectedRegion.replace(/\s+/g, '_')+"&key="+battutaKey)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                cityLoaded: true,
+                cityList: result,
+              });
+              console.log(this.state.cityList)
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                regionLoaded: true,
                 error
               });
             }
@@ -80,16 +142,46 @@ class App extends Component {
   }
 
   render() {
-    const { error, listLoaded, passLoaded, nextPass, countryList } = this.state;
+    const { error, countryLoaded, regionLoaded, cityLoaded, passLoaded, nextPass, countryList, regionList, cityList, selectedCountry, selectedRegion, selectedCity } = this.state;
+    const optionsCountry = countryList.map(item => {
+      if (countryList) {
+        const tempArray = Object.values(item);
+        return <option value={tempArray} key={item.code}>{item.name}</option>
+      }
+    })
+    const optionsRegion = regionList.map(item => {
+      if (regionList) {
+        return <option value={item} key={item}>{item}</option>
+      }
+    })
+    const optionsCity = cityList.map(item => {
+      if (cityList) {
+        return <option value={item} key={item.city}>{item.city}</option>
+      }
+    })
     if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!listLoaded || !passLoaded) {
+    } else if (!countryLoaded || !regionLoaded || !cityLoaded || !passLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <div>
-        <div>{<CountryMenu list={countryList}/>}</div>
-        <div>{<FlyoverCalc city="Beijing" next={nextPass}/>}</div>
+        <div Grid container>
+          <div Grid item xs>
+            <select value={selectedCountry} onChange={this.handleCountry}>
+              {optionsCountry}
+            </select>
+          </div>
+          <div Grid item xs>
+            <select value={selectedRegion} onChange={this.handleRegion}>
+              {optionsRegion}
+            </select>
+          </div>
+          <div Grid item xs>
+            <select value={selectedCity} onChange={this.handleCity}>
+              {optionsCity}
+            </select>
+          </div>
+          <div Grid item xs={12}>{<FlyoverCalc city={selectedCity.name} lat={selectedCity.latitude} long={selectedCity.longitude} next={nextPass}/>}</div>
         </div>
       );
     }
--- src/FlyoverCalc.js
+++ src/FlyoverCalc.js
@@ -17,6 +17,7 @@ class FlyoverCalc extends Component {
   render() {
       return (
         <div>
+        <h1>{this.props.city} is at {this.props.lat}, {this.props.long}</h1>
         <h2>The ISS will be over {this.props.city} next at {this.state.nextPass}</h2>
         </div>
       );
