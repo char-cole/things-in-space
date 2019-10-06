4fea29af14ddf3a0a7f686cbffdc15735e14dd46
charcole <charcole@pm.me>
2019-10-06 18:03:32 -0500

    Updates

---
+++ precache-manifest.31e763a4eed98abd288249c428592415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1717a95bc909a833e68b",
+    "url": "/static/js/main.e6e4a7a2.chunk.js"
+  },
+  {
+    "revision": "bdf30eebc25158c447a2",
+    "url": "/static/js/2.f13c05ae.chunk.js"
+  },
+  {
+    "revision": "1717a95bc909a833e68b",
+    "url": "/static/css/main.797d3eb2.chunk.css"
+  },
+  {
+    "revision": "85ea69cd2ba92c7ba9a5712b63303cc4",
+    "url": "/index.html"
+  }
+];
